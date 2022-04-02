09d729396f6481e9ab5a1b788424bb951b6f4a84
AnastasiaSoldatkina <94635746+AnastasiaSoldatkina@users.noreply.github.com>
2022-04-03 01:57:42 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -24,38 +24,42 @@ int calcul(char w, int x, int y) {
 }
 std::string infx2pstfx(std::string inf) {
 std::string str;
+    char probel = ' ';
     TStack <char, 100> stackA;
-    int i = 0;
-    while (i <= inf.length()) {
-        if (Imp(inf[i]) == 0 && (Imp(inf[i]) != 1) && Imp(inf[i]) != 10) {
-            stackA.push(inf[i]);
-        } else if (Imp(inf[i]) > Imp(stackA.get()) && (Imp(inf[i]) != 1) && Imp(inf[i]) != 10) {
-            stackA.push(inf[i]);
-        } else if (stackA.isEmpty() && (Imp(inf[i]) != 1) && Imp(inf[i]) != 10) {
-            stackA.push(inf[i]);
-        } else if (Imp(inf[i]) == 10) {
-                str.push_back(inf[i]);
-                str.push_back(' ');
-        } else if (Imp(inf[i]) <= Imp(stackA.get()) && Imp(inf[i]) != 10 && Imp(inf[i]) != 1) {
-            while (Imp(inf[i]) <= Imp(stackA.get()) && !stackA.isEmpty() && Imp(stackA.get()) !=0) {
-                str.push_back(stackA.get());
+    for (int i = 0; i < inf.size(); i++) {
+        if (Imp(inf[i]) == 4) {
+            str.push_back(inf[i]);
+            str.push_back(probel);
+        } else if (Imp(inf[i]) == 0  && stackA.isEmpty()) {
+                stackA.push(inf[i]);
+            } else if ((Imp(inf[i]) > Imp(stackA.get()))) {
+                stackA.push(inf[i]);
+            } else if (Imp(inf[i]) == 1) {
+                while (Imp(stackA.get()) != 0) {
+                    str.push_back(stackA.get());
+                    str.push_back(probel);
+                    stackA.pop();
+                }
                 stackA.pop();
-                str.push_back(' ');
+            } else {
+                while (!stackA.isEmpty()
+                    && (Imp(inf[i]) <= Imp(stackA.get()))) {
+                    str.push_back(stackA.get());
+                    str.push_back(probel);
+                    stackA.pop();
+                }
+                stackA.push(inf[i]);
             }
-            stackA.push(inf[i]);
-        } else if (Imp(inf[i]) == 1) {
-            while (Imp(stackA.get()) != 0) {
-                str.push_back(stackA.get());
-                str.push_back(' ');
-                stackA.pop();
-            }
-            stackA.pop();
         }
-        i++;
-    } while (!stackA.isEmpty()) {
+    }
+    while (!stackA.isEmpty()) {
         str.push_back(stackA.get());
-        str.push_back(' ');
+        str.push_back(probel);
         stackA.pop();
+    }
+    for (int i = 0; i < str.size(); i++) {
+        if (str[str.size() - 1] == ' ')
+            str.erase(str.size() - 1);
     }
     return str;
 }
