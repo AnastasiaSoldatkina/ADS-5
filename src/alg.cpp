33326c7990510fb9f3a6399835fe4ab7aa910ea8
AnastasiaSoldatkina <94635746+AnastasiaSoldatkina@users.noreply.github.com>
2022-04-02 22:56:21 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -22,31 +22,29 @@ int calcul(char w, int x, int y) {
     }
 }
 std::string infx2pstfx(std::string inf) {
-std::string str;
+ std::string str;
     TStack <char, 100> stackA;
     int i = 0;
     while (i <= inf.length()) {
         if ((Imp(inf[i]) == 0 || (Imp(inf[i]) > Imp(stackA.get())) || stackA.isEmpty()) && (Imp(inf[i]) != 1) && Imp(inf[i]) != 10) {
             stackA.push(inf[i]);
-        } else {
-            if (Imp(inf[i]) == 10) {
+        } else if (Imp(inf[i]) == 10) {
                 str.push_back(inf[i]);
                 str.push_back(' ');
-            } if (Imp(inf[i]) <= Imp(stackA.get()) && Imp(inf[i]) != 10 && Imp(inf[i]) != 1) {
+            } else if (Imp(inf[i]) <= Imp(stackA.get()) && Imp(inf[i]) != 10 && Imp(inf[i]) != 1) {
                 while (Imp(inf[i]) <= Imp(stackA.get()) && !stackA.isEmpty()) {
                     str.push_back(stackA.get());
                     stackA.pop();
                     str.push_back(' ');
                 }
                 stackA.push(inf[i]);
-            } if (Imp(inf[i]) == 1) {
+            } else if (Imp(inf[i]) == 1) {
                 while (Imp(stackA.get()) != 0) {
                     str.push_back(stackA.get());
                     str.push_back(' ');
                     stackA.pop();
                 }
-                stackA.pop();
-            }
+            stackA.pop();
         }
         i++;
     } while (!stackA.isEmpty()) {
@@ -68,7 +66,7 @@ int eval(std::string pref) {
             if (pref[j] != ' ') {
                 stackB.push(pref[j] - '0');
             }
-        } if (Imp(pref[j]) < 4) {
+        } else if (Imp(pref[j]) < 4) {
             x = stackB.get();
             stackB.pop();
             y = stackB.get();
