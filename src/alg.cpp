81e2eee3e3b8a6fb759fd23de2177533656c1920
AnastasiaSoldatkina <94635746+AnastasiaSoldatkina@users.noreply.github.com>
2022-04-03 01:27:26 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -19,6 +19,7 @@ int calcul(char w, int x, int y) {
     case '/': return x / y;
     case '-': return x - y;
     case '+': return x + y;
+    default: return 10;
     }
 }
 std::string infx2pstfx(std::string inf) {
@@ -26,24 +27,28 @@ std::string str;
     TStack <char, 100> stackA;
     int i = 0;
     while (i <= inf.length()) {
-        if ((Imp(inf[i]) == 0 || (Imp(inf[i]) > Imp(stackA.get())) || stackA.isEmpty()) && (Imp(inf[i]) != 1) && Imp(inf[i]) != 10) {
+        if (Imp(inf[i]) == 0 && (Imp(inf[i]) != 1) && Imp(inf[i]) != 10) {
+            stackA.push(inf[i]);
+        } else if (Imp(inf[i]) > Imp(stackA.get()) && (Imp(inf[i]) != 1) && Imp(inf[i]) != 10) {
+            stackA.push(inf[i]);
+        } else if (stackA.isEmpty() && (Imp(inf[i]) != 1) && Imp(inf[i]) != 10) {
             stackA.push(inf[i]);
         } else if (Imp(inf[i]) == 10) {
                 str.push_back(inf[i]);
                 str.push_back(' ');
-} else if (Imp(inf[i]) <= Imp(stackA.get()) && Imp(inf[i]) != 10 && Imp(inf[i]) != 1) {
-                while (Imp(inf[i]) <= Imp(stackA.get()) && !stackA.isEmpty()) {
-                    str.push_back(stackA.get());
-                    stackA.pop();
-                    str.push_back(' ');
-                }
-                stackA.push(inf[i]);
-            } else if (Imp(inf[i]) == 1) {
-                while (Imp(stackA.get()) != 0) {
-                    str.push_back(stackA.get());
-                    str.push_back(' ');
-                    stackA.pop();
-                }
+        }else if (Imp(inf[i]) <= Imp(stackA.get()) && Imp(inf[i]) != 10 && Imp(inf[i]) != 1) {
+            while (Imp(inf[i]) <= Imp(stackA.get()) && !stackA.isEmpty() && Imp(stackA.get()) !=0) {
+                str.push_back(stackA.get());
+                stackA.pop();
+                str.push_back(' ');
+            }
+            stackA.push(inf[i]);
+        } else if (Imp(inf[i]) == 1) {
+            while (Imp(stackA.get()) != 0) {
+                str.push_back(stackA.get());
+                str.push_back(' ');
+                stackA.pop();
+            }
             stackA.pop();
         }
         i++;
